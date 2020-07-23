85a34a340604eaa2971c0092748b6974e8351046
Sanek <thesyom4egg@gmail.com>
2020-07-23 18:02:38 +0300

    1

--- WebApplication5/Controllers/HomeController.cs
+++ WebApplication5/Controllers/HomeController.cs
@@ -4,11 +4,21 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Text;
+using System.IO;
 
 namespace WebApplication5.Controllers
 {
     public class HomeController : Controller
     {
+        char[] characters = new char[] { 'А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ё', 'Ж', 'З', 'И',
+                                            'Й', 'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С',
+                                            'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ь', 'Ы', 'Ъ',
+                                            'Э', 'Ю', 'Я', ' ', '1', '2', '3', '4', '5', '6', '7',
+                                            '8', '9', '0' };
+
+        private int N; //длина алфавита
+        public string key = "";
+
         public ActionResult Index()
         {
             return View();
@@ -47,12 +57,23 @@ namespace WebApplication5.Controllers
             if (upload != null)
             {
                 // получаем имя файла
-                string fileName = System.IO.Path.GetFileName(upload.FileName);
+                //string fileName = System.IO.Path.GetFileName(upload.FileName);
+                string fileName = "forCrypt";
                 // сохраняем файл в папку Files в проекте
                 upload.SaveAs(Server.MapPath("~/Files/" + fileName));
             }
             return RedirectToAction("Index");
         }
+
+        [HttpPost]
+        //public ActionResult Show(string parameterName)
+        //{
+        //    // тут что то делаешь с этим параметром.
+        //    Response.Write("<script>alert('" + parameterName + "')</script>");
+        //    key = parameterName;
+        //    return View();
+
+        //}
 
 
         public ActionResult Download()
@@ -66,8 +87,66 @@ namespace WebApplication5.Controllers
             return RedirectToAction("Index");
         }
 
+        public ActionResult Someclick(string parameterName)
+        {
+            N = characters.Length;
+            key = parameterName;
+            
+            if (key.Length > 0)//textBoxKeyWord.Text.Length > 0)
+            {
+                string s;
+                Response.Write("<script>alert('" + key + "')</script>");
+
+                StreamReader sr = new StreamReader(Server.MapPath("~/Files/in.txt"));
+                StreamWriter sw = new StreamWriter(Server.MapPath("~/Files/out.txt"));
+
+                while (!sr.EndOfStream)
+                {
+                    s = sr.ReadLine();
+                    sw.WriteLine(Encode(s, key));
+                    // sw.Write(WebApplication5.Controllers.HomeController.Encode(s, textBoxKeyWord.Text), true, System.Text.Encoding.GetEncoding("windows-1251"));
+                }
 
 
+                sr.Close();
+                sw.Close();
+            }
+            else if ( key == null)
+            {
+                Response.Write("<script>alert('Заполните поле ключ!')</script>");
+            }
+            else
+            {
+                Response.Write("<script>alert('Заполните поле ключ!')</script>");
+            }
 
+            //return View();
+            return RedirectToAction("Index");
+        }
+
+        public string Encode(string input, string keyword)
+        {
+            input = input.ToUpper();
+            keyword = keyword.ToUpper();
+
+            string result = "";
+
+            int keyword_index = 0;
+
+            foreach (char symbol in input)
+            {
+                int c = (Array.IndexOf(characters, symbol) +
+                    Array.IndexOf(characters, keyword[keyword_index])) % N;
+
+                result += characters[c];
+
+                keyword_index++;
+
+                if ((keyword_index + 1) == keyword.Length)
+                    keyword_index = 0;
+            }
+
+            return result;
+        }
     }
 }
